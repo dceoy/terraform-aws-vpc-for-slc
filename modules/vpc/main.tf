d03b100a97951fdabdc07a1f145b2a9411b0f73b
dceoy <dnarsil+github@gmail.com>
2024-01-07 08:38:56 +0900

    Create a VPC flow log

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -34,7 +34,7 @@ locals {
   )
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -47,11 +47,73 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_flow_log" "flow_log" {
+  iam_role_arn    = aws_iam_role.vpc_logs_iam_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_logs_group.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.vpc.id
+  tags = {
+    Name        = "${var.project_name}-${var.env_type}-vpc-flow-logs"
+    ProjectName = var.project_name
+    EnvType     = var.env_type
+  }
+}
+
+resource "aws_cloudwatch_log_group" "vpc_logs_group" {
+  name              = "/aws/vpc-flow-logs/${aws_vpc.vpc.id}"
+  retention_in_days = 14
+  tags = {
+    Name        = "${var.project_name}-${var.env_type}-vpc-logs-group"
+    ProjectName = var.project_name
+    EnvType     = var.env_type
+  }
+}
+
+resource "aws_iam_role" "vpc_logs_iam_role" {
+  name = "${var.project_name}-${var.env_type}-vpc-logs-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["sts:AssumeRole"],
+        Effect = "Allow",
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+  inline_policy {
+    name = "vpc-logs-policy"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:DescribeLogGroups",
+            "logs:DescribeLogStreams"
+          ],
+          Effect   = "Allow",
+          Resource = aws_cloudwatch_log_group.vpc_logs_group.arn
+        }
+      ]
+    })
+  }
+  tags = {
+    Name        = "${var.project_name}-${var.env_type}-vpc-logs-role"
+    ProjectName = var.project_name
+    EnvType     = var.env_type
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
   count                   = length(local.availability_zones)
   cidr_block              = var.private_subnet_cidrs[count.index]
   availability_zone       = local.availability_zones[count.index]
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.vpc.id
   map_public_ip_on_launch = false
   tags = {
     Application = "${var.project_name}-${var.env_type}-subnet-private${count.index}"
@@ -64,10 +126,25 @@ resource "aws_subnet" "private" {
 
 output "vpc_id" {
   description = "VPC ID"
-  value = aws_vpc.main.id
+  value       = aws_vpc.vpc.id
 }
 
 output "private_subnet_ids" {
-  description = "Private Subnet IDs"
-  value = aws_subnet.private[*].id
+  description = "Private subnet IDs"
+  value       = aws_subnet.private_subnet[*].id
+}
+
+output "vpc_flow_log" {
+  description = "VPC flow log"
+  value       = aws_flow_log.flow_log.id
+}
+
+output "vpc_flow_log_group" {
+  description = "VPC flow log group"
+  value       = aws_cloudwatch_log_group.vpc_logs_group.name
+}
+
+output "vpc_flow_log_iam_role" {
+  description = "VPC flow log IAM role"
+  value       = aws_iam_role.vpc_logs_iam_role.name
 }
