bcbcd0fe5a1c211215fe47f62fe82ed11163e4f5
dceoy <dceoy@users.noreply.github.com>
2025-03-23 01:25:16 +0900

    Remove unused local variables

--- modules/vpc/local.tf
+++ modules/vpc/local.tf
@@ -1,9 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
 locals {
-  account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
-  vpc_name   = "${var.system_name}-${var.env_type}-vpc"
+  vpc_name = "${var.system_name}-${var.env_type}-vpc"
 }
