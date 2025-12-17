0e7f3b5ae5270e988d8cb0227b5016acd16277a2
dceoy <1938249+dceoy@users.noreply.github.com>
2025-12-18 04:35:39 +0900

    Update modules/s3/outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -3,14 +3,44 @@ output "io_s3_bucket_id" {
   value       = contains(keys(aws_s3_bucket.storage), "io") ? aws_s3_bucket.storage["io"].id : null
 }
 
+output "io_s3_bucket_domain_name" {
+  description = "IO S3 bucket domain name"
+  value       = contains(keys(aws_s3_bucket.storage), "io") ? aws_s3_bucket.storage["io"].bucket_domain_name : null
+}
+
+output "io_s3_bucket_regional_domain_name" {
+  description = "IO S3 bucket regional domain name"
+  value       = contains(keys(aws_s3_bucket.storage), "io") ? aws_s3_bucket.storage["io"].bucket_regional_domain_name : null
+}
+
 output "awslogs_s3_bucket_id" {
   description = "AWS service logs S3 bucket ID"
   value       = contains(keys(aws_s3_bucket.storage), "awslogs") ? aws_s3_bucket.storage["awslogs"].id : null
 }
 
+output "awslogs_s3_bucket_domain_name" {
+  description = "AWS service logs S3 bucket domain name"
+  value       = contains(keys(aws_s3_bucket.storage), "awslogs") ? aws_s3_bucket.storage["awslogs"].bucket_domain_name : null
+}
+
+output "awslogs_s3_bucket_regional_domain_name" {
+  description = "AWS service logs S3 bucket regional domain name"
+  value       = contains(keys(aws_s3_bucket.storage), "awslogs") ? aws_s3_bucket.storage["awslogs"].bucket_regional_domain_name : null
+}
+
 output "s3logs_s3_bucket_id" {
   description = "AWS service logs S3 bucket ID"
   value       = contains(keys(aws_s3_bucket.storage), "s3logs") ? aws_s3_bucket.storage["s3logs"].id : null
+}
+
+output "s3logs_s3_bucket_domain_name" {
+  description = "AWS service logs S3 bucket domain name"
+  value       = contains(keys(aws_s3_bucket.storage), "s3logs") ? aws_s3_bucket.storage["s3logs"].bucket_domain_name : null
+}
+
+output "s3logs_s3_bucket_regional_domain_name" {
+  description = "AWS service logs S3 bucket regional domain name"
+  value       = contains(keys(aws_s3_bucket.storage), "s3logs") ? aws_s3_bucket.storage["s3logs"].bucket_regional_domain_name : null
 }
 
 output "s3_iam_policy_arn" {
