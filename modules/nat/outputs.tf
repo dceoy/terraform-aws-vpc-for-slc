518aa652faf005e7cdf830822bd5e7e72fc40462
dceoy <1938249+dceoy@users.noreply.github.com>
2026-02-14 04:07:46 +0900

    Fix modules/nat/outputs.tf

--- modules/nat/outputs.tf
+++ modules/nat/outputs.tf
@@ -1,36 +1,24 @@
-output "nat_gateway_ids" {
-  description = "NAT Gateway IDs"
-  value       = aws_nat_gateway.nat[*].id
+output "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  value       = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].id : null
 }
 
-output "auto_provision_zones" {
-  description = "Auto provision zones reported by regional NAT gateway addresses"
-  value = compact(flatten([
-    for nat in aws_nat_gateway.nat : [
-      for address in nat.nat_gateway_addresses : try(tomap(address)["auto_provision_zones"], null)
-    ]
-  ]))
+output "nat_auto_provision_zones" {
+  description = "Auto provision zones reported by regional NAT gateway"
+  value       = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].auto_provision_zones : null
 }
 
-output "auto_scaling_ips" {
-  description = "Auto scaling IPs reported by regional NAT gateway addresses"
-  value = compact(flatten([
-    for nat in aws_nat_gateway.nat : [
-      for address in nat.nat_gateway_addresses : try(tomap(address)["auto_scaling_ips"], null)
-    ]
-  ]))
+output "nat_auto_scaling_ips" {
+  description = "Auto scaling IPs reported by regional NAT gateway"
+  value       = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].auto_scaling_ips : null
 }
 
-output "regional_nat_gateway_address" {
-  description = "Regional NAT gateway addresses"
-  value = flatten([
-    for nat in aws_nat_gateway.nat : [
-      for address in nat.nat_gateway_addresses : try(tomap(address)["regional_nat_gateway_address"], null)
-    ]
-  ])
+output "nat_gateway_address" {
+  description = "Regional NAT gateway address"
+  value       = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].regional_nat_gateway_address : null
 }
 
-output "route_table_id" {
-  description = "Route table IDs associated with NAT routes"
-  value       = aws_route.nat[*].route_table_id
+output "nat_route_table_id" {
+  description = "ID of the route table created for the NAT gateway"
+  value       = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].route_table_id : null
 }
